b658113181282a13a4e1657aaf4fc5b2a16122ff
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2020-03-11 08:46:30 -0300

    Initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<h1>Footer aqui</h1>
+
+<?php wp_footer(); ?>
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php wp_title('-',true,'right'); bloginfo(); ?></title>
+	
+	<?php wp_head(); ?>
+
+	<!--<link href="https://fonts.googleapis.com/css?family=Raleway:500" rel="stylesheet">-->
+
+	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/images/favicon.png" type="image/x-icon">
+
+	
+</head>
+<body>
