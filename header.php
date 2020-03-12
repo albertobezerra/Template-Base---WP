5886b812cc06306725f1af02387560c23c7a44bc
Alberto Bezerra <43380864+albertobezerra@users.noreply.github.com>
2020-03-12 11:50:59 -0300

    Final

--- header.php
+++ header.php
@@ -5,10 +5,10 @@
 	
 	<?php wp_head(); ?>
 
-	<!--<link href="https://fonts.googleapis.com/css?family=Raleway:500" rel="stylesheet">-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
 	<link rel="shortcut icon" href="<?php bloginfo( 'template_directory' ); ?>/assets/images/favicon.png" type="image/x-icon">
+	
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
 
 	
 </head>
